ab33dfa045f03a41ccb6804b8b148372cd850143
Eric <zamalchi@techsquare.com>
2016-11-15 13:44:51 -0500

    fixing hoursapp centos7 installer

--- hoursapp/centos7-installer.sh
+++ hoursapp/centos7-installer.sh
@@ -2,15 +2,17 @@
 
 INSTALL_PATH=${1}
 
-sudo yum install python git
+sudo yum -y install python git
 
-sudo yum install python-pip
+sudo yum -y install python-pip
 sudo pip install --upgrade pip
 
 sudo pip install markdown
-sudo yum install pycrypto
+sudo yum -y install pycrypto
 
 git clone https://www.github.com/zamalchi/hoursapp.git ${INSTALL_PATH}
+
+chown ${USER}:${USER} ${INSTALL_PATH}
 
 cd ${INSTALL_PATH}
 ./launcher.sh
